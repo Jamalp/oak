cf0eff2bf6f4bb6f7edc3b3339b070b31e9b67a0
Jamal Powell <jamal.powell@domanistudios.com>
2017-06-27 14:37:11 -0700

    switch to craft, initial testing

--- public/index.php
+++ public/index.php
@@ -1,17 +1,19 @@
 <?php
-/**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
- *
- * @package WordPress
- */
 
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define('WP_USE_THEMES', true);
+// Path to your craft/ folder
+$craftPath = '../craft';
 
-/** Loads the WordPress Environment and Template */
-require( dirname( __FILE__ ) . '/wp-blog-header.php' );
+// Do not edit below this line
+$path = rtrim($craftPath, '/').'/app/index.php';
+
+if (!is_file($path))
+{
+	if (function_exists('http_response_code'))
+	{
+		http_response_code(503);
+	}
+
+	exit('Could not find your craft/ folder. Please ensure that <strong><code>$craftPath</code></strong> is set correctly in '.__FILE__);
+}
+
+require_once $path;
